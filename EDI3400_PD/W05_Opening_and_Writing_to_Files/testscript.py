7e39efa880e3f6c6d1849cede676c7475157e50c
chiachiamaru <chiakinishihara.jp@gmail.com>
2023-09-20 13:19:46 +0200

    Update

--- EDI3400_PD/W05_Opening_and_Writing_to_Files/testscript.py
+++ EDI3400_PD/W05_Opening_and_Writing_to_Files/testscript.py
@@ -1,10 +1,1 @@
-# Your code goes here
-user_input = int(input("Enter a number: "))
-total = 0
-number = 1
-
-while number < user_input:
-    total += number
-    number += 1
-
-print(f"The sum of numbers from 1 to {user_input} is {total}.")
+print("Hi, this is from test script")
