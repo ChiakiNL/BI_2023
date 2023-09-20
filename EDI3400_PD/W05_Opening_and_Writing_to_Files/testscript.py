9af87826a55f9895e984e58477e7392a8503795e
chiachiamaru <chiakinishihara.jp@gmail.com>
2023-09-20 13:07:32 +0200

    Update

--- EDI3400_PD/W05_Opening_and_Writing_to_Files/testscript.py
+++ EDI3400_PD/W05_Opening_and_Writing_to_Files/testscript.py
@@ -1,1 +1,10 @@
-print('Hello world!')
+# Your code goes here
+user_input = int(input("Enter a number: "))
+total = 0
+number = 1
+
+while number < user_input:
+    total += number
+    number += 1
+
+print(f"The sum of numbers from 1 to {user_input} is {total}.")
